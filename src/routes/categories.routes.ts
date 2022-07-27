c6477955afbf33f736f5d2b3b8850fcc21b7eb7f
Dickson-Pinheiro <62717698+Dickson-Pinheiro@users.noreply.github.com>
2022-07-27 02:17:14 -0300

    configurando docker e inicio do orm

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,16 +9,15 @@ const categoriesRoutes = Router()
 const upload = multer({dest: './tmp'})
 
 categoriesRoutes.post('/', (request, response) =>{
- return createCategoryController.handle(request, response);
+    return createCategoryController.handle(request, response);
 })
 
 categoriesRoutes.post('/import', upload.single("file") , (request, response) => {
-
     return importCategoryController.handle(request, response)
 })
 
 categoriesRoutes.get('/', (request, response) => {
-        return listCategoryController.handle(request, response)
+    return listCategoryController.handle(request, response)
 })
 
 export  {categoriesRoutes }
